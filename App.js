f4d7d87a5b395874a288b4275b917134f01d7f4a
LLTae2 <taehyun5820@gmail.com>
2022-10-26 17:46:05 +0900

    Update::홈화면 라우팅 추가

--- App.js
+++ App.js
@@ -6,15 +6,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignupScreen from './components/Signup';
 import MainScreen from './components/Main';
+import HomeScreen from './components/Home';
+
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Main">
         <Stack.Screen name="Login" component={LoginScreen}/>
         <Stack.Screen name="Signup" component={SignupScreen}/>
         <Stack.Screen name="Main" component={MainScreen}/>
+        <Stack.Screen name="Home" component={HomeScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
