9e49c5969efdc961a7ab9c66c3283684b8951a04
LLTae2 <taehyun5820@gmail.com>
2022-11-15 23:29:58 +0900

    Update::회원가입 구현

--- components/Signup.js
+++ components/Signup.js
@@ -1,73 +1,126 @@
 import React, { useEffect, useState } from "react";
-import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { NavigationActions, withNavigation } from "react-navigation";
 import { users } from "../assets/export";
 
 const Signup = ({ navigation }) => {
-  const [userList, setUserList] = useState(users);
-
-  const check = (username) => {
-    for (let i = 0; i < userList.length; i++) {
-      if (userList[i].username === username) return false;
-    }
-    return true;
-  };
-
   const [signupData, setSignupData] = useState({
     id: "",
     password: "",
     passwordCheck: "",
   });
+  const [userList, setUserList] = useState(users);
+  const [checkId, setCheckId] = useState(false);
+  const [checkPW, setCheckPW] = useState(false);
+  const [complete, setComplete] = useState(false);
+
+  const check = () => {
+    if (signupData.id === "minse") {
+      setCheckId(true);
+    } else {
+      setCheckId(false);
+    }
+    if (signupData.password !== signupData.passwordCheck) {
+      setCheckPW(true);
+    } else {
+      setCheckPW(false);
+    }
+    if (!checkId && !checkPW) {
+      setComplete(true);
+    }
+  };
+
   return (
     <View style={styles.main_box}>
-      <View style={styles.main_login_box}>
-        <Text style={styles.main_logo}>GWH</Text>
-        <Text style={styles.login_text}>아이디</Text>
-        <TextInput
-          onChangeText={(e) => {
-            setSignupData({ ...signupData, id: e });
-          }}
-          style={styles.input_box}
-        />
-        <Text style={styles.signup_text}>비밀번호</Text>
-        <TextInput
-          onChangeText={(e) => {
-            setSignupData({ ...signupData, password: e });
-          }}
-          style={styles.input_box}
-        />
-        <Text style={styles.signup_text}>비밀번호 확인</Text>
-        <TextInput
-          onChangeText={(e) => {
-            setSignupData({ ...signupData, passwordCheck: e });
-          }}
-          style={styles.input_box}
-        />
-        <Text
-          style={styles.login_button}
-          onPress={(e) => {
-            // if (check(signupData.id)) {
-            if (signupData.password === signupData.passwordCheck)
-              navigation.navigate("Login");
-            else Alert("비밀번호를 다시 확인해주세요.");
-            //  else Alert("동일한 아이디가 이미 존재합니다.");
-          }}
-        >
-          회원가입하기
-        </Text>
-        <View style={styles.isMember}>
-          <Text style={styles.isMember_text}>이미 계정이 있으신가요?</Text>
-          <Text
+      {complete ? (
+        <>
+          <Text style={{ fontSize: 30 }}>회원가입 완료!</Text>
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#ABC9FF",
+              borderRadius: 300,
+              width: 140,
+              alignSelf: "center",
+              marginTop: 10,
+            }}
             onPress={() => {
-              // this.props.navigation.navigate('Login')
               navigation.navigate("Login");
             }}
-            style={styles.isMember_button}
           >
-            로그인하기
+            <Text
+              style={{
+                fontSize: 20,
+                color: "#065FD4",
+                alignSelf: "center",
+              }}
+            >
+              로그인하기
+            </Text>
+          </TouchableOpacity>
+        </>
+      ) : (
+        <View style={styles.main_login_box}>
+          <Text style={styles.main_logo}>GWH</Text>
+          <Text style={styles.login_text}>아이디</Text>
+          <TextInput
+            onChangeText={(e) => {
+              setSignupData({ ...signupData, id: e });
+            }}
+            style={styles.input_box}
+          />
+          {checkId ? (
+            <Text style={{ fontSize: 10, color: "red" }}>
+              동일한 아이디가 존재합니다.
+            </Text>
+          ) : (
+            ""
+          )}
+          <Text style={styles.signup_text}>비밀번호</Text>
+          <TextInput
+            onChangeText={(e) => {
+              setSignupData({ ...signupData, password: e });
+            }}
+            style={styles.input_box}
+          />
+          <Text style={styles.signup_text}>비밀번호 확인</Text>
+          <TextInput
+            onChangeText={(e) => {
+              setSignupData({ ...signupData, passwordCheck: e });
+            }}
+            style={styles.input_box}
+          />
+          {checkPW ? (
+            <Text style={{ fontSize: 10, color: "red" }}>
+              비밀번호를 다시 확인해주세요.
+            </Text>
+          ) : (
+            ""
+          )}
+          <Text style={styles.login_button} onPress={check}>
+            회원가입하기
           </Text>
+          <View style={styles.isMember}>
+            <Text style={styles.isMember_text}>이미 계정이 있으신가요?</Text>
+            <Text
+              onPress={() => {
+                // this.props.navigation.navigate('Login')
+                navigation.navigate("Login");
+              }}
+              style={styles.isMember_button}
+            >
+              로그인하기
+            </Text>
+          </View>
         </View>
-      </View>
+      )}
     </View>
   );
 };
